72d0f3c77206117609fc397fa277c26020b00ff2
alitahaozturk <alitahaozturkk@gmail.com>
2021-07-25 19:01:04 +0300

    dashboard ok

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -1,8 +1,9 @@
 import { Movie } from "./movie";
 
 export const Movies: Movie[] = [
-  {id : 1, name : "movie 1"},
-  {id : 2, name : "movie 2"},
-  {id : 3, name : "movie 3"}
+  {id : 1, name : "movie 1", description :"acıklama", imageUrl : "1.png"},
+  {id : 2, name : "movie 2" , description :"acıklama", imageUrl : "2.jpg"},
+  {id : 3, name : "movie 3", description :"acıklama", imageUrl : "3.jpg"},
+  {id : 4, name : "movie 4", description :"acıklama", imageUrl : "4.jpg"}
 
 ]
